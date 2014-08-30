049f28ba36a040ae05869f12cb122a8004f87940
Eric McNiece <emcniece@gmail.com>
2014-08-30 12:16:26 -0700

    Adding Spark IDE compatible headers

--- firmware/examples/example.ino
+++ firmware/examples/example.ino
@@ -1,11 +1,33 @@
-#include <Wire.h>
-#include <Adafruit_MPL115A2.h>
+/**************************************************************************/
+/*!
+    @file     Adafruit_MPL115A2.cpp
+    @author   E.McNiece (EMC2 Innovation)
+    @license  BSD (see license.txt)
+
+    Example for the MPL115A2 barometric pressure sensor
+
+    This is a library for the Adafruit MPL3115A2 breakout
+    ----> https://www.adafruit.com/products/992
+
+    Adafruit invests time and resources providing this open source code,
+    please support Adafruit and open-source hardware by purchasing
+    products from Adafruit!
+
+    @section  HISTORY
+
+    v1.0.1 - Ported to Spark Core
+    v1.0 - First release
+*/
+/**************************************************************************/
+
+#include <Adafruit_MPL115A2/Adafruit_MPL115A2.h>
 
 Adafruit_MPL115A2 mpl115a2;
 
 void setup(void) 
 {
   Serial.begin(9600);
+  while(!Serial.available()) SPARK_WLAN_Loop();	// needed for Spark init
   Serial.println("Hello!");
   
   Serial.println("Getting barometric pressure ...");
